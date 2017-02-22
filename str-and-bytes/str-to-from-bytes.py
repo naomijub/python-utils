5748bd0dba296c638c56f4d355ba2cb333457923
naomijub <jnboeira@outlook.com>
2017-02-22 00:41:19 -0300

    Test for str to byte working / test for byte to str

--- str-and-bytes/str-to-from-bytes.py
+++ str-and-bytes/str-to-from-bytes.py
@@ -1,14 +1,27 @@
 import unittest
 
+class Converter:
+    def __init__(self):
+        pass
+
+    def convert(self, strg):
+        return strg.encode('utf-8')
+
 class Test(unittest.TestCase):
     def setUp(self):
-        pass
+        self.conv = Converter()
 
     def tearDown(self):
         pass
 
-    def test_empy_str_return_bytes(self):
-        assert converter(" ") == None
+    def test_empty_str_return_empty_bytes(self):
+        assert self.conv.convert(" ") == b' '
+
+    def test_car_str_return_bytes(self):
+        assert self.conv.convert("car") == b'car'
+
+    def test_empty_bytes_return_empty_str(self):
+        assert self.conv.convert(b' ') == " "
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
