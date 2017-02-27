94ff9398a46a7d1a110f18cb8e47d8578e3fd61b
naomijub <jnboeira@outlook.com>
2017-02-26 22:08:38 -0300

    test for 0,0

--- numbers/coprime.py
+++ numbers/coprime.py
@@ -1,14 +1,21 @@
 import unittest
 
+class Coprime:
+    def __init__(self):
+        pass
+
+    def isCoprime(self, a, b):
+        return False
+
 class Test(unittest.TestCase):
     def setUp(self):
-        pass
+        self.cop = Coprime()
 
     def tearDown(self):
         pass
 
     def test_if_0_and_0_are_coprime(self):
-        assert self.coprime.isCoprime(0, 0) == false
+        assert self.cop.isCoprime(0,0) == False
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
